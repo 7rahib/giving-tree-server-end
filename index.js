7d10d56362a53ba15ba3098e1c3b100730aaa27f
7rahib <rahibrashid002@gmail.com>
2022-09-08 03:24:07 +0600

    volunteer profile update api created

--- index.js
+++ index.js
@@ -70,6 +70,18 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24d' });
             res.send({ result, token });
         })
+        // Updating individual valunteer data
+        app.put('/volunteer/:email', async (req, res) => {
+            const email = req.params.email;
+            const updateProfile = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: updateProfile,
+            };
+            const result = await volunteersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
         // Storing volunteer data in database
         app.put('/organizations/:email', async (req, res) => {
             const email = req.params.email;
